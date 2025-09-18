610f616809e1a286f4d17ab1afbb631fd95a2788
YixingZhengAu <Yixing.Zheng.Au@gmail.com>
2025-09-18 21:11:09 +1000

    first commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dab_project",
+    version="0.0.1",
+    description="This contains code in the ./src dirctory of the project",
+    author="Yixing Zheng",
+    packages=find_packages(where="./src"),
+    package_dir={"": "./src"},
+    install_requires=["setuptools"],
+    entry_points={
+        "packages": [
+            "main = dab_project.main:main"
+        ]
+    }
+)
